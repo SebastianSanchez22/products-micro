0cc4cd8ac995d870c6d485ee629e8880fcaf56c7
Sebastián Sánchez Orrego <sebastiansaor22@gmail.com>
2024-11-01 13:06:04 -0500

    fix(products): update columns constraints

--- src/entities/models/products.py
+++ src/entities/models/products.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Numeric
+from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -8,11 +8,11 @@ class TblProducts(Base):
     __tablename__ = "tblProducts"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(30), nullable=False)
-    category = Column(String(40), nullable=False)
-    price = Column(Numeric(7, 2), nullable=False)
+    name = Column(String(50), nullable=False)
+    category = Column(String(50), nullable=False)
+    price = Column(Float, nullable=False)
     stock = Column(Integer, nullable=False)
-    image = Column(String(100), nullable=True)
+    image = Column(String(50), nullable=True)
 
     def as_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
