8db97811883dae0c60a44c692a698bf3076e9952
Sebastián Sánchez Orrego <sebastiansaor22@gmail.com>
2024-11-01 13:39:21 -0500

    fix(routing): update orders and products routing

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
 
+from src.functions.products import products_router
+from src.functions.orders import orders_router
+
 app = FastAPI()
 main_router = APIRouter()
 
@@ -12,3 +15,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+main_router.include_router(orders_router)
+main_router.include_router(products_router)
+
+app.include_router(main_router)
